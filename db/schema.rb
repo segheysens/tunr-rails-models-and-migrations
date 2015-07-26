4a7e5b4101eea8a53f16f7841176893d7fefad4e
Jesse Shawl <jesse@jshawl.com>
2015-07-26 11:19:47 -0400

    add timestamps to artists

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726150324) do
+ActiveRecord::Schema.define(version: 20150726150946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "artists", force: :cascade do |t|
-    t.string "name"
-    t.string "photo_url"
-    t.string "nationality"
+    t.string   "name"
+    t.string   "photo_url"
+    t.string   "nationality"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "songs", force: :cascade do |t|
